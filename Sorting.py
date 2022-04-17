035ee11b146e53765f3544be6a275976813c0450
stingraymir <owaismir2437@gmail.com>
2022-04-17 15:43:29 +0530

    ypo change

--- Sorting.py
+++ Sorting.py
@@ -1,55 +1,55 @@
 #implement a BUBBLE SORT
 
-def bubbleSort(lst):
-    for i in range(len(lst)):
-        for j in range(len(lst)-1):
-            if lst[j] > lst[j+1]:
-                lst[j], lst[j+1] = lst[j+1], lst[j]
-    return lst
+def bubbleSort(list):
+    for i in range(len(list)):
+        for j in range(len(list)-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+    return list
 
 
 #implement a SELECTION SORT
-def selectionSort(lst):
-    for i in range(len(lst)):
+def selectionSort(list):
+    for i in range(len(list)):
         min_index = i
-        for j in range(i+1, len(lst)):
-            if lst[j] < lst[min_index]:
+        for j in range(i+1, len(list)):
+            if list[j] < list[min_index]:
                 min_index = j
-        lst[i], lst[min_index] = lst[min_index], lst[i]
-    return lst
+        list[i], list[min_index] = list[min_index], list[i]
+    return list
 
 #implement a INSERTION SORT
-def insertionSort(lst):
-    for i in range(1, len(lst)):
+def insertionSort(list):
+    for i in range(1, len(list)):
         j = i
-        while j > 0 and lst[j] < lst[j-1]:
-            lst[j], lst[j-1] = lst[j-1], lst[j]
+        while j > 0 and list[j] < list[j-1]:
+            list[j], list[j-1] = list[j-1], list[j]
             j -= 1
-    return lst
+    return list
 
 #Implement a MERGE SORT
-def mergeSort(lst):
-    if len(lst) <= 1:
-        return lst
-    mid = len(lst) // 2
-    left = lst[:mid]
-    right = lst[mid:]
+def mergeSort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    left = list[:mid]
+    right = list[mid:]
     left = mergeSort(left)
     right = mergeSort(right)
     return (left + right)
 
 #Implement a QUICK SORT
-def quickSort(lst):
-    if len(lst) <= 1:
-        return lst
-    pivot = lst[0]
+def quickSort(list):
+    if len(list) <= 1:
+        return list
+    pivot = list[0]
     left = []
     right = []
-    for i in range(1, len(lst)):
-        if lst[i] < pivot:
-            left.append(lst[i])
+    for i in range(1, len(list)):
+        if list[i] < pivot:
+            left.append(list[i])
         else:
-            right.append(lst[i])
+            right.append(list[i])
     return quickSort(left) + [pivot] + quickSort(right)
 
 
